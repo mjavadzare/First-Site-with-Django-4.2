88906aa1ab184b6e05d57e13e53500280b294f02
Mohammad Javad Zare <mjz589.2018@gmail.com>
2023-05-08 00:59:05 +0330

    database section is now hidden

--- BrandNewSite/setting/prod.py
+++ BrandNewSite/setting/prod.py
@@ -17,20 +17,29 @@ ALLOWED_HOSTS = ['themikey.ir','www.themikey.ir']
 # sites framework
 SITE_ID = 3
 
-CSRF_COOKIE_SECURE = True
-SESSION_COOKIE_SECURE = True
-SECURE_SSL_REDIRECT = True
+## X-XSS-Protection
+SECURE_BROWSER_XSS_FILTER = True
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'themikey_mikey',
-        'USER': 'themikey_javad',
-        'PASSWORD': 'zX2Yj(8*u!x3',
-        'HOST': 'localhost',
-        'PORT': '3306',
-    }
-}
+## X-Frame-Options
+X_FRAME_OPTIONS = 'SAMEORIGIN'
+#X-Content-Type-Options
+SECURE_CONTENT_TYPE_NOSNIFF = True
+## Strict-Transport-Security
+SECURE_HSTS_SECONDS = 15768000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+
+## that requests over HTTP are redirected to HTTPS. aslo can config in webserver
+SECURE_SSL_REDIRECT = True 
+
+# for more security
+CSRF_COOKIE_SECURE = True
+CSRF_USE_SESSIONS = True
+CSRF_COOKIE_HTTPONLY = True
+SESSION_COOKIE_SECURE = True
+SESSION_COOKIE_SAMESITE = 'Strict'
+
+DATABASES = config("DATABASES")
 
 STATIC_ROOT = '/home/themikey/public_html/static'
 MEDIA_ROOT = '/home/themikey/public_html/media' 
